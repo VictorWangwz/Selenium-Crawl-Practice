7ca6c9ab1394f1e666908db38a2da2296e78078a
Gordon Seto <gordon.seto@gmail.com>
2017-07-11 13:44:32 -0700

    Renamed files

--- mortgageboss.py
+++ mortgageboss.py
@@ -1,7 +1,6 @@
 import os
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
